c61ab16bf60dfe792b32d5e158f6b7d9042e6389
aalaa2045 <39449588+aalaa2045@users.noreply.github.com>
2020-10-12 16:12:53 +0300

    Final Edition...

--- app/src/main/java/com/example/bitwisee/MainActivity.kt
+++ app/src/main/java/com/example/bitwisee/MainActivity.kt
@@ -15,56 +15,31 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 class MainActivity : AppCompatActivity() {
     var changed : Boolean = false
     var staticValue:Int = 0
-    var staticValueInput:Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
     }
     fun myfunc2(view : View){
         changed = true
-        val inpt = findViewById<EditText>(R.id.value)
-        var value = inpt.getText().toString()
-        if(value.isEmpty()){
-            value = "0"
-        }
-
-        staticValueInput = value.toInt()
-
     }
     fun myfunc(view:View){
-        val position = (view.tag as String).toInt()
-        var num : Int
-        val bool = (view as Switch).isChecked
         val inpt = findViewById<EditText>(R.id.value)
-        var value = inpt.getText().toString()
-        var value2 = staticValue
-        if(value.isEmpty()){
-            value = "0"
-        }
+        var value = inpt.getText().toString().let{ if(it.isEmpty())  "0" else it }
 
-        if(changed){
-            value = "$staticValue"
-        }
-
-        num = if(bool){
-            set(value.toInt(),position)
+        val position = (view.tag as String).toInt()
+        staticValue = if((view as Switch).isChecked){
+            set(staticValue,position)
         }else{
-            clear(value.toInt(),position)
+            clear(staticValue,position)
         }
-
-        staticValue = num
         if(changed){
-            if(staticValue == staticValueInput){
+            if(value.toInt() == staticValue){
                 findViewById<ConstraintLayout>(R.id.bg).setBackgroundColor(Color.parseColor("#FF0000"))
                 changed = false
-
             }
         }else{
-            inpt.setText("$num")
+            inpt.setText("$staticValue")
             findViewById<ConstraintLayout>(R.id.bg).setBackgroundColor(Color.parseColor("#FFFFFF"))
-
         }
     }
     fun set(inpt:Int,position:Int):Int{
